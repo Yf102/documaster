0248e62dd08fce3bbdb8b28c82d07b8f8e3a9914
Filip Hristov <philipx192@gmail.com>
2018-06-28 16:09:20 +0300

    [db-propel] Added propel library for ORM - Created script for initialising poster db - Git ignored propel config and base folders (created on propel init) - Created initial database schema - Added provisional data for initial propel configuration

---
+++ scripts/reset-db-pgsql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! -d vendor ]; then
+    echo "Please exec $0 from source root directory";
+    exit;
+fi
+
+PROPEL_PARAMS="--platform=pgsql --sql-dir=src/orm/sql/pgsql/"
+
+# Prepare config file, PHP models, sql db
+vendor/bin/propel config:convert
+vendor/bin/propel model:build
+vendor/bin/propel sql:build
+
+# Create insert init values and migrate all changes
+vendor/bin/propel sql:insert $PROPEL_PARAMS
+vendor/bin/propel migrate
---
+++ src/orm/sql/pgsql/provision.sql
@@ -0,0 +1,2 @@
+INSERT INTO public."USERS" ("USER_PASS", "USER_EMAIL", "FIRST_NAME", "LAST_NAME", "USER_PHONE")
+VALUES ('Ecp123', 'admin@documaster.com', 'Filip', 'Hristov', 123456789);
