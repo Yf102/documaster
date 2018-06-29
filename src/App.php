b49242fd6c883b04dd94d573cd805407d8240711
Filip Hristov <philipx192@gmail.com>
2018-06-29 13:46:04 +0300

    [library] Added an application class for getting current user

---
+++ src/App.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: filiphristov
+ * Date: 29/06/2018
+ * Time: 12:03
+ */
+
+class DocumasterApp
+{
+	private $_curUser = null;
+
+	public function curUser()
+	{
+		// have loaded the user already?
+		if (!empty($this->_curUser)) {
+			return $this->_curUser;
+		}
+		// load user info based on session or API auth
+		$email = isset($_SESSION['user_mail']) ? $_SESSION['user_mail'] : null;
+		if (empty($email)) {
+			return null;
+		}
+
+		$this->_curUser =	\Documaster\UsersQuery::create()->findOneByUserEmail($email);
+		return $this->_curUser;
+	}
+}
