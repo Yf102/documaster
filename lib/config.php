75bc62d24d90168994646a47e7bd47e5f79a9578
Filip Hristov <philipx192@gmail.com>
2018-06-29 11:57:05 +0300

    [lib] Added a config library php file

---
+++ lib/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: filiphristov
+ * Date: 28/06/2018
+ * Time: 19:23
+ */
+
+define('ROOT_DIR', dirname(__DIR__));
+define('PUB_DIR', ROOT_DIR.'/public/');
+define('LIB_DIR', ROOT_DIR.'/lib/');
+define('SRC_DIR', ROOT_DIR . '/src');
