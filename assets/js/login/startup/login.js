2b3accdb6f4324d90a433c748a95ea4ca8af712c
Filip Hristov <philipx192@gmail.com>
2018-06-29 19:22:53 +0300

    [i18n] Added ui functionality to be able to set desired language

--- assets/js/login/startup/login.js
+++ assets/js/login/startup/login.js
@@ -1,6 +1,40 @@
 import React from 'react';
+import {RegisterTranslations} from '../../services/Translation/RegisterTranslations.js';
+import {Config} from "../../services/Config/index.js";
+import {LocaleSwitcher} from "../../services/Config/components/LocaleSwitcher/index.js";
+import {Storage} from "../../services/Storage/index.js";
+import Translate from 'react-translate-component';
+
+RegisterTranslations.run();
 
 class Login extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			config: Storage.get('app-config') || { config: {locale: 'en'} }
+		};
+
+		this.updateConfigState = this.updateConfigState.bind(this);
+	}
+
+	updateConfigState(config) {
+		this.setState({config});
+	}
+
+	render() {
+		return (
+			<div>
+				<Config updateConfigState={this.updateConfigState} config={this.state.config}>
+					<LocaleSwitcher config={this.state.config} />
+					<LoginForm />
+				</Config>
+			</div>
+		)
+	}
+}
+
+class LoginForm extends React.Component {
 	render() {
 		let errorMSG = "";
 		if (this.props.error) {
@@ -8,19 +42,18 @@ class Login extends React.Component {
 		}
 
 		let last_user = this.props.last_username ? this.props.last_username : undefined;
-		return (
-			<div>
-				<form className="login-form" action={this.props.path} method="post">
-					{errorMSG}
-					<label htmlFor="username">Username:</label>
-					<input type="text" id="username" name="_username" defaultValue={last_user}/>
 
-					<label htmlFor="password">Password:</label>
-					<input type="password" id="password" name="_password"/>
+		return(
+			<form className="login-form" action={this.props.path} method="post">
+				{errorMSG}
+				<label htmlFor="username"><Translate content="username_lbl" />:</label>
+				<input type="text" id="username" name="_username" defaultValue={last_user}/>
 
-					<button type="submit">Login</button>
-				</form>
-			</div>
+				<label htmlFor="password"><Translate content="password_lbl" />:</label>
+				<input type="password" id="password" name="_password"/>
+
+				<button type="submit"><Translate content="login_btn" /></button>
+			</form>
 		)
 	}
 }
